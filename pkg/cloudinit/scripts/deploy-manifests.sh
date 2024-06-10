28e3f0a32f9e5976c6308fea1ddb7ca88d9a9b95
Angelos Kolaitis <neoaggelos@gmail.com>
2024-06-10 20:28:57 +0300

    more bash fixes

--- pkg/cloudinit/scripts/deploy-manifests.sh
+++ pkg/cloudinit/scripts/deploy-manifests.sh
@@ -4,6 +4,6 @@
 ## - k8s is installed and bootstrapped.
 ## - /opt/capi/manifests/ is a directory with YAML manifests to deploy once on the cluster.
 
-for file in find /opt/capi/manifests/ -name '*.yaml' | sort; do
+for file in $(find /opt/capi/manifests/ -name '*.yaml' | sort); do
   k8s kubectl apply -f "$file"
 done
