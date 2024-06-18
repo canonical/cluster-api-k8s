5ba4eaaddadf241d85bc61e2efcf15b80922424d
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-06-18 14:54:16 +0200

    add --file flag to join-cluster script

--- pkg/cloudinit/scripts/join-cluster.sh
+++ pkg/cloudinit/scripts/join-cluster.sh
@@ -6,6 +6,7 @@
 ## - /capi/etc/join-token is a valid join token
 
 address="$(cat /capi/etc/microcluster-address)"
+config_file="/capi/etc/config.yaml"
 token="$(cat /capi/etc/join-token)"
 
-k8s join-cluster "${token}" --address "${address}"
+k8s join-cluster "${token}" --address "${address}" --file "${config_file}"
