4d687c518d2c8252991571fe849a56d353715241
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-08-08 15:33:47 +0200

    add wrapper scripts

--- hack/build-e2e-images.sh
+++ hack/build-e2e-images.sh
@@ -1,3 +1,14 @@
-cd ../templates/docker
-sudo docker build  -t k8s-snap:dev-old --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.29.6
+#!/bin/bash
+
+# Description:
+#   Build k8s-snap docker images required for e2e tests.
+#
+# Usage:
+#   ./build-e2e-images.sh
+
+DIR="$(realpath "$(dirname "${0}")")"
+
+cd "${DIR}/../templates/docker"
+sudo docker build . -t k8s-snap:dev-old --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.29.6
 sudo docker build . -t k8s-snap:dev-new --build-arg BRANCH=main
+cd -
