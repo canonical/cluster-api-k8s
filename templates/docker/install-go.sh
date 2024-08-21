322ba4b02bf55d4fcc2a6a4ee68f83c1c28234fc
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-08-21 19:09:40 -0400

    review comment

--- templates/docker/install-go.sh
+++ templates/docker/install-go.sh
@@ -3,10 +3,11 @@
 # Author: Angelos Kolaitis <angelos.kolaitis@canonical.com>
 #
 # Usage:
-#    $ install-go.sh
+#    $ install-go.sh $version
 #
-# Description: Download go 1.22.6 and install under /usr/local/go
+# Description: Download go version and install under /usr/local/go
 
+VERSION="$1"
 wget "https://go.dev/dl/go1.22.6.linux-amd64.tar.gz"
 tar -C /usr/local -xvzf "go1.22.6.linux-amd64.tar.gz"
-rm "go1.22.6.linux-amd64.tar.gz"
+rm "go${VERSION}.linux-amd64.tar.gz"
