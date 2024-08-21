218952cd794ad637ac40fa953b472682c67d55fb
eaudetcobello <etienne.audet-cobello@canonical.com>
2024-08-20 22:07:39 -0400

    hardcode go version

--- templates/docker/install-go.sh
+++ templates/docker/install-go.sh
@@ -3,13 +3,10 @@
 # Author: Angelos Kolaitis <angelos.kolaitis@canonical.com>
 #
 # Usage:
-#    $ install-go.sh 1.22
+#    $ install-go.sh
 #
-# Description: Download latest go version and install under /usr/local/go
+# Description: Download go 1.22.6 and install under /usr/local/go
 
-VERSION="$1"
-
-fname="$(curl -s https://go.dev/dl/ | grep -oP "go$VERSION\.\d+\.linux-amd64\.tar\.gz" | head -1)"
-wget "https://go.dev/dl/$fname"
-tar -C /usr/local -xvzf "$fname"
-rm "$fname"
+wget "https://go.dev/dl/go1.22.6.linux-amd64.tar.gz"
+tar -C /usr/local -xvzf "go1.22.6.linux-amd64.tar.gz"
+rm "go1.22.6.linux-amd64.tar.gz"
