1728168027c24bb0366f9c556458d32747f144ec
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-04 16:06:13 +0200

    pin kubernetes version for new image

--- hack/build-e2e-images.sh
+++ hack/build-e2e-images.sh
@@ -10,5 +10,5 @@ DIR="$(realpath "$(dirname "${0}")")"
 
 cd "${DIR}/../templates/docker"
 sudo docker build . -t k8s-snap:dev-old --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.29.6
-sudo docker build . -t k8s-snap:dev-new --build-arg BRANCH=main
+sudo docker build . -t k8s-snap:dev-new --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.30.4
 cd -
