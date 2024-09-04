a3ac03ac0a58ca33547b92e21c074db468744ed8
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-09-04 11:40:01 -0400

    Add Github action

--- hack/ci-e2e-tests.sh
+++ hack/ci-e2e-tests.sh
@@ -2,20 +2,54 @@
 
 set -xe
 
-readonly HACK_DIR="$(realpath $(dirname "${0}"))"
+# This script is used to run e2e tests for the CK8s CAPI.
+# It sets up an LXD container, installs the CK8s management cluster, and runs e2e tests.
+# The goal is to test the CK8s provider with different infrastructure providers (e.g., AWS, Azure, GCP). Only AWS is supported for now.
+# The script should be able to run on any Linux machine with LXD installed.
 
+# USAGE
+# ./hack/ci-e2e-tests.sh [SKIP_CLEANUP] [INFRA_PROVIDER] [CK8S_PROVIDER_VERSION]
+# SKIP_CLEANUP: Optional. If set to "true", the LXD container and cloud provider resources will not be deleted after the tests are run. Default is "true".
+# INFRA_PROVIDER: Optional. The infrastructure provider to use. Default is "aws".
+# CK8S_PROVIDER_VERSION: Optional. The CK8s provider version to use. Default is "v0.1.2".
+
+readonly HACK_DIR="$(realpath $(dirname "${0}"))"
 cd "$HACK_DIR"
 
 readonly SKIP_CLEANUP=${1:-true}
-readonly
+readonly INFRA_PROVIDER=${2:-aws}
+readonly CK8S_PROVIDER_VERSION=${3:-v0.1.2}
+
 readonly LXD_CHANNEL="5.21/stable"
 readonly LXC_IMAGE="ubuntu:20.04"
 readonly K8S_PROFILE_URL="https://raw.githubusercontent.com/canonical/k8s-snap/main/tests/integration/lxd-profile.yaml"
 readonly K8S_PROFILE_PATH="/tmp/k8s.profile"
 readonly CONTAINER_NAME="k8s-test"
 
-# User specific paths
-readonly USER_CREDENTIALS_PATH="/home/user/.creds"
+# Utility function for printing errors to stderr
+function error_exit {
+  printf "ERROR: %s\n" "$1" >&2
+  return 1
+}
+
+# Check that all required environment variables are set
+function check_required_env_vars {
+  local required_env_vars=()
+
+  if [[ $INFRA_PROVIDER == "aws" ]]; then
+    required_env_vars+=("AWS_REGION" "AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY")
+  fi
+
+  for var in "${required_env_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+      error_exit "Missing required environment variable: $var"
+    fi
+  done
+}
+
+function exec_in_container {
+  lxc exec $CONTAINER_NAME -- bash -c "$1"
+}
 
 # Install LXD snap
 function install_lxd {
@@ -35,71 +69,103 @@ function setup_lxd_profile {
 # Setup and configure the container
 function setup_container {
   lxc launch $LXC_IMAGE $CONTAINER_NAME -p default -p k8s
-  sleep 3 # Wait for the container to be up and running
-  lxc exec $CONTAINER_NAME -- bash -c "apt update && apt install -y snapd"
-  sleep 1
-  lxc exec $CONTAINER_NAME -- bash -c "systemctl start snapd"
+  until exec_in_container true; do
+    sleep 1
+  done
+
+  exec_in_container "apt update && apt install -y snapd"
+  exec_in_container "systemctl start snapd"
+}
+
+function configure_container_env {
+  if [[ $INFRA_PROVIDER == "aws" ]]; then
+    # Check for clusterawsadm binary
+    exec_in_container "which clusterawsadm" || error_exit "clusterawsadm binary not found in container"
+
+    set +x
+    lxc config set $CONTAINER_NAME environment.AWS_REGION "$AWS_REGION"
+    lxc config set $CONTAINER_NAME environment.AWS_SECRET_ACCESS_KEY "$AWS_SECRET_ACCESS_KEY"
+    lxc config set $CONTAINER_NAME environment.AWS_ACCESS_KEY_ID "$AWS_ACCESS_KEY_ID"
+
+    local aws_creds
+    aws_creds=$(lxc exec "$CONTAINER_NAME" -- bash -c "clusterawsadm bootstrap credentials encode-as-profile")
+
+    lxc config set "$CONTAINER_NAME" environment.AWS_B64ENCODED_CREDENTIALS "$aws_creds"
+    set -x
+  fi
 }
 
 # Main installation and configuration
 function setup_management_cluster {
-  lxc exec $CONTAINER_NAME -- bash -c "snap install k8s --classic --edge"
+  sleep 5
+  exec_in_container "snap install k8s --classic --edge"
   sleep 1
-  lxc exec $CONTAINER_NAME -- bash -c "snap install go --classic"
-  lxc exec $CONTAINER_NAME -- bash -c "mkdir -p /root/.kube"
-  lxc exec $CONTAINER_NAME -- bash -c "sudo k8s bootstrap"
-  lxc exec $CONTAINER_NAME -- bash -c "sudo k8s status --wait-ready"
-  lxc exec $CONTAINER_NAME -- bash -c "sudo k8s config > /root/.kube/config"
+  exec_in_container "snap install go --classic"
+  exec_in_container "mkdir -p /root/.kube"
+  exec_in_container "sudo k8s bootstrap"
+  exec_in_container "sudo k8s status --wait-ready"
+  exec_in_container "sudo k8s config > /root/.kube/config"
 }
 
 # Transfer and execute scripts
 function install_tools {
-  for script in install-clusterctl.sh install-clusterctlawsadm.sh install-aws-nuke.sh write-provider-config.sh; do
-    lxc file push ./$script $CONTAINER_NAME/root/$script
-    if [[ ! $script == "write-provider-config.sh" ]]; then
-      lxc exec $CONTAINER_NAME -- bash -c "chmod +x /root/$script && /root/$script"
-    else
-      lxc exec $CONTAINER_NAME -- bash -c "mkdir -p /root/.cluster-api"
-      lxc exec $CONTAINER_NAME -- bash -c "chmod +x /root/$script && /root/$script /root/.cluster-api/clusterctl.yaml v0.1.2"
-    fi
-  done
+  tools=(install-clusterctl.sh)
 
-  lxc file push $USER_CREDENTIALS_PATH $CONTAINER_NAME/root/.creds
-  lxc exec $CONTAINER_NAME -- bash -c 'echo "source /root/.creds" >/root/.bashrc'
+  if [[ $INFRA_PROVIDER == "aws" ]]; then
+    tools+=(install-clusterctlawsadm.sh install-aws-nuke.sh)
+  fi
+
+  for script in "${tools[@]}"; do
+    lxc file push ./"$script" $CONTAINER_NAME/root/"$script"
+    exec_in_container "chmod +x /root/$script && /root/$script"
+  done
 }
 
 function init_clusterctl {
-  lxc exec $CONTAINER_NAME -- bash -c "source /root/.creds && clusterctl init -i aws -b ck8s:v0.1.2 -c ck8s:v0.1.2 --config /root/.cluster-api/clusterctl.yaml"
+  configure_container_env # Ensures that the right environment variables are set in the container
+
+  lxc file push ./write-provider-config.sh $CONTAINER_NAME/root/write-provider-config.sh
+  exec_in_container "chmod +x /root/write-provider-config.sh"
+  exec_in_container "mkdir -p /root/.cluster-api"
+  exec_in_container "/root/write-provider-config.sh /root/.cluster-api/clusterctl.yaml $CK8S_PROVIDER_VERSION"
+
+  exec_in_container "clusterctl init -i $INFRA_PROVIDER -b ck8s:$CK8S_PROVIDER_VERSION -c ck8s:$CK8S_PROVIDER_VERSION --config /root/.cluster-api/clusterctl.yaml"
 }
 
 function run_e2e_tests {
-  make GINKGO.FOCUS="Workload cluster creation" test-e2e
-}
-
-function write_aws_nuke_config {
-  lxc exec $CONTAINER_NAME -- bash -c "mkdir -p /root/.aws-nuke"
-  lxc exec $CONTAINER_NAME -- bash -c "echo ""$AWS_NUKE_CONFIG"" > /root/.aws-nuke/config.yaml"
+  make USE_EXISTING_CLUSTER=true GINKGO_FOCUS="Workload cluster creation" test-e2e
 }
 
 function cleanup {
-  write_aws_nuke_config
-  lxc exec $CONTAINER_NAME -- bash -c "aws-nuke --config /root/.aws-nuke/config.yaml --force"
+  if [[ $SKIP_CLEANUP == "true" ]]; then
+    return
+  fi
+
+  # Infra-specific cleanup
+  if [[ $INFRA_PROVIDER == "aws" ]]; then
+    exec_in_container "mkdir -p /root/.aws-nuke"
+    exec_in_container "echo ""$AWS_NUKE_CONFIG"" > /root/.aws-nuke/config.yaml"
+    exec_in_container "aws-nuke --config /root/.aws-nuke/config.yaml --force"
+  fi
+
   lxc delete $CONTAINER_NAME --force
 }
 
 function main {
+  if [[ $INFRA_PROVIDER != "aws" ]]; then
+    error_exit "Unsupported infrastructure provider: $INFRA_PROVIDER"
+    exit 1
+  fi
+
+  check_required_env_vars
   install_lxd
   setup_lxd_profile
   setup_container
   setup_management_cluster
   install_tools
   init_clusterctl
-  run_e2e_tests
-
-  if [[ $SKIP_CLEANUP == "false" ]]; then
-    write_aws_nuke_config
-    cleanup
-  fi
+  #run_e2e_tests
+  cleanup
 }
 
 main
