1ce27e7f7da7475a6efe77bc4404cb7233c15564
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-09-03 21:43:11 -0400

    Make install script more robust

--- hack/install-clusterctlawsadm.sh
+++ hack/install-clusterctlawsadm.sh
@@ -10,7 +10,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v0.0.0/clusterawsadm-linux-amd64 -o clusterawsadm
+curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v2.6.1/clusterawsadm-linux-amd64 -o clusterawsadm
 chmod +x ./clusterawsadm
 sudo mv ./clusterawsadm /usr/local/bin
 
