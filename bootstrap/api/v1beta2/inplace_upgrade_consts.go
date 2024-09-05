310d83d14599de066fcbe3b637b39724601f0051
Berkay Tekin Oz <berkay.tekinoz@canonical.com>
2024-09-05 08:20:24 +0000

    Add api version to annotation prefix

--- bootstrap/api/v1beta2/inplace_upgrade_consts.go
+++ bootstrap/api/v1beta2/inplace_upgrade_consts.go
@@ -1,10 +1,10 @@
 package v1beta2
 
 const (
-	InPlaceUpgradeToAnnotation        = "k8sd.io/in-place-upgrade-to"
-	InPlaceUpgradeStatusAnnotation    = "k8sd.io/in-place-upgrade-status"
-	InPlaceUpgradeReleaseAnnotation   = "k8sd.io/in-place-upgrade-release"
-	InPlaceUpgradeRefreshIDAnnotation = "k8sd.io/in-place-upgrade-refresh-id"
+	InPlaceUpgradeToAnnotation        = "v1beta2.k8sd.io/in-place-upgrade-to"
+	InPlaceUpgradeStatusAnnotation    = "v1beta2.k8sd.io/in-place-upgrade-status"
+	InPlaceUpgradeReleaseAnnotation   = "v1beta2.k8sd.io/in-place-upgrade-release"
+	InPlaceUpgradeRefreshIDAnnotation = "v1beta2.k8sd.io/in-place-upgrade-refresh-id"
 )
 
 const (
