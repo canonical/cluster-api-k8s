fbdfd54f4f20236c93fe7fb5cddc91b3522a04fb
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-10-08 07:41:07 -0400

    retry 5 times instead of infinite

--- pkg/cloudinit/scripts/configure-snapstore-proxy.sh
+++ pkg/cloudinit/scripts/configure-snapstore-proxy.sh
@@ -6,29 +6,49 @@
 #   - /capi/etc/snapstore-proxy-domain contains the snapstore domain
 #   - /capi/etc/snapstore-proxy-id contains the snapstore id
 
+if [ ! -s /capi/etc/snapstore-proxy-scheme ] || [ ! -s /capi/etc/snapstore-proxy-domain ] || [ ! -s /capi/etc/snapstore-proxy-id ]; then
+  echo "Missing or empty snapstore proxy configuration files, exiting."
+  exit 1
+fi
+
 SNAPSTORE_PROXY_SCHEME=$(cat /capi/etc/snapstore-proxy-scheme)
 SNAPSTORE_PROXY_DOMAIN=$(cat /capi/etc/snapstore-proxy-domain)
 SNAPSTORE_PROXY_ID=$(cat /capi/etc/snapstore-proxy-id)
 
-if [ -z "${SNAPSTORE_PROXY_SCHEME}" ] || [ -z "${SNAPSTORE_PROXY_DOMAIN}" ] || [ -z "${SNAPSTORE_PROXY_ID}" ]; then
-  echo "Missing snapstore proxy configuration"
-  exit 1
-fi
-
 if ! type -P curl; then
+  count=0
   while ! snap install curl; do
-    echo "Failed to install curl, will retry"
+    count=$((count + 1))
+    if [ $count -gt 5 ]; then
+      echo "Failed to install curl, exiting."
+      exit 1
+    fi
+    echo "Failed to install curl, retrying ($count/5)"
     sleep 5
   done
 fi
 
+count=0
 while ! curl -sL "${SNAPSTORE_PROXY_SCHEME}"://"${SNAPSTORE_PROXY_DOMAIN}"/v2/auth/store/assertions | snap ack /dev/stdin; do
-  echo "Failed to ACK store assertions, will retry"
+  count=$((count + 1))
+  if [ $count -ge 5 ]; then
+    echo "Failed to ACK store assertions, exiting."
+    exit 1
+  fi
+
+  echo "Failed to ACK store assertions, retrying ($count/5)"
   sleep 5
 done
 
+count=0
 while ! snap set core proxy.store="${SNAPSTORE_PROXY_ID}"; do
-  echo "Failed to configure snapd with store ID, will retry"
+  count=$((count + 1))
+  if [ $count -ge 5 ]; then
+    echo "Failed to configure snapd with store ID, exiting."
+    exit 1
+  fi
+
+  echo "Failed to configure snapd with store ID, retrying ($count/5)"
   sleep 5
 done
 
