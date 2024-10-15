3453cf8f495644c583366914cdc0e91567ad422a
Mateo Florido <mateo.florido@canonical.com>
2024-10-15 18:26:14 -0500

    Remove errors

--- pkg/ck8s/workload_cluster.go
+++ pkg/ck8s/workload_cluster.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	apiv1 "github.com/canonical/k8s-snap-api/api/v1"
-	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
