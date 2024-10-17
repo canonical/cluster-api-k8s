f198d4dff2bb036d6ddb6982a2d7491e6adf1e2e
Maciej Golaszewski <maciej.golaszewski@canonical.com>
2024-10-17 17:35:18 +0200

    shell script fix

--- pkg/cloudinit/scripts/configure-proxy.sh
+++ pkg/cloudinit/scripts/configure-proxy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 # Assumptions:
 #   - runs before install k8s
@@ -10,22 +10,22 @@ ENVIRONMENT_FILE="/etc/environment"
 
 
 
-if [ -f HTTP_PROXY_FILE ]; then
-  local HTTP_PROXY=$(cat HTTP_PROXY_FILE)
+if [ -f ${HTTP_PROXY_FILE} ]; then
+  HTTP_PROXY=$(cat ${HTTP_PROXY_FILE})
   echo "http_proxy=${HTTP_PROXY}" >> "${ENVIRONMENT_FILE}"
   echo "HTTP_PROXY=${HTTP_PROXY}" >> "${ENVIRONMENT_FILE}"
 fi
 
 
-if [ -f HTTPS_PROXY_FILE ]; then
-  local HTTPS_PROXY=$(cat HTTPS_PROXY_FILE)
+if [ -f ${HTTPS_PROXY_FILE} ]; then
+  HTTPS_PROXY=$(cat ${HTTPS_PROXY_FILE})
   echo "https_proxy=${HTTPS_PROXY}" >> "${ENVIRONMENT_FILE}"
   echo "HTTPS_PROXY=${HTTPS_PROXY}" >> "${ENVIRONMENT_FILE}"
 fi
 
 
-if [ -f NO_PROXY_FILE ]; then
-  local NO_PROXY=$(cat NO_PROXY_FILE)
+if [ -f ${NO_PROXY_FILE} ]; then
+  NO_PROXY=$(cat ${NO_PROXY_FILE})
   echo "no_proxy=${NO_PROXY}" >> "${ENVIRONMENT_FILE}"
   echo "NO_PROXY=${NO_PROXY}" >> "${ENVIRONMENT_FILE}"
 fi
