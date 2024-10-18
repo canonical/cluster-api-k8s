1dabf63fbc534188f1c79a5e92f104ac8ea5c100
Maciej Golaszewski <maciej.golaszewski@canonical.com>
2024-10-18 20:20:01 +0200

    review fixes2

--- pkg/cloudinit/scripts/configure-proxy.sh
+++ pkg/cloudinit/scripts/configure-proxy.sh
@@ -3,12 +3,10 @@
 # Assumptions:
 #   - runs before install k8s
 
-HTTP_PROXY_FILE="/capi/etc/http-proxy"
-HTTPS_PROXY_FILE="/capi/etc/https-proxy"
-NO_PROXY_FILE="/capi/etc/no-proxy"
-ENVIRONMENT_FILE="/etc/environment"
-
-
+HTTP_PROXY_FILE="/tmp/capi/etc/http-proxy"
+HTTPS_PROXY_FILE="/tmp/capi/etc/https-proxy"
+NO_PROXY_FILE="/tmp/capi/etc/no-proxy"
+ENVIRONMENT_FILE="/tmp/etc/environment"
 
 if [ -f ${HTTP_PROXY_FILE} ]; then
   HTTP_PROXY=$(cat ${HTTP_PROXY_FILE})
@@ -16,13 +14,11 @@ if [ -f ${HTTP_PROXY_FILE} ]; then
   echo "HTTP_PROXY=${HTTP_PROXY}" >> "${ENVIRONMENT_FILE}"
 fi
 
-
 if [ -f ${HTTPS_PROXY_FILE} ]; then
   HTTPS_PROXY=$(cat ${HTTPS_PROXY_FILE})
   echo "https_proxy=${HTTPS_PROXY}" >> "${ENVIRONMENT_FILE}"
   echo "HTTPS_PROXY=${HTTPS_PROXY}" >> "${ENVIRONMENT_FILE}"
 fi
-
 
 if [ -f ${NO_PROXY_FILE} ]; then
   NO_PROXY=$(cat ${NO_PROXY_FILE})
