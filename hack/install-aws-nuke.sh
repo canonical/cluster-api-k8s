8e7d62d32d25053bb0a08f742d7225eab684f68e
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-10-18 09:11:11 -0400

    improve aws-nuke cleanup

--- hack/install-aws-nuke.sh
+++ hack/install-aws-nuke.sh
@@ -10,10 +10,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-curl -L https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-amd64.tar.gz -o aws-nuke-v2.25.0-linux-amd64.tar.gz
-tar -xvf aws-nuke-v2.25.0-linux-amd64.tar.gz -C /tmp
-rm aws-nuke-v2.25.0-linux-amd64.tar.gz
-chmod +x /tmp/aws-nuke-v2.25.0-linux-amd64
-mv /tmp/aws-nuke-v2.25.0-linux-amd64 /usr/local/bin/aws-nuke
+curl -L https://github.com/ekristen/aws-nuke/releases/download/v3.28.0/aws-nuke-v3.28.0-linux-amd64.tar.gz -o aws-nuke-v3.28.0-linux-amd64.tar.gz
+tar -xvf aws-nuke-v3.28.0-linux-amd64.tar.gz -C /tmp
+rm aws-nuke-v3.28.0-linux-amd64.tar.gz
+chmod +x /tmp/aws-nuke
+mv /tmp/aws-nuke /usr/local/bin/aws-nuke
 
 aws-nuke version
