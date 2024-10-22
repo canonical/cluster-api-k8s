2994cd22339f4570481a53cd13b2652bc23da3b9
Etienne Audet-Cobello <etienne.audet-cobello@canonical.com>
2024-10-21 21:27:01 -0400

    try juju

---
+++ hack/run-e2e-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+snap install go --classic --channel 1.22/stable
+snap install kubectl --classic --channel 1.31/stable
+
+apt update
+apt install -y docker.io docker-buildx make
+systemctl enable --now docker
+
+curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/v2.6.1/clusterawsadm-linux-amd64 -o clusterawsadm
+chmod +x ./clusterawsadm
+mv ./clusterawsadm /usr/local/bin
+clusterawsadm version
+
+wget https://github.com/kubernetes-sigs/kind/releases/download/v0.24.0/kind-linux-amd64 -O /usr/local/bin/kind
+
+export KIND_EXPERIMENTAL_DOCKER_NETWORK=bridge
+kind version
+
+git clone git@github.com:canonical/cluster-api-k8s.git /home/ubuntu/cluster-api-k8s && (cd /home/ubuntu/cluster-api-k8s || exit 1)
+
+sudo -E E2E_INFRA=aws GINKGO_FOCUS="Workload cluster creation" SKIP_RESOURCE_CLEANUP=true make test-e2e
