6c3b35b8b3a673c846db9384e3578f408474a41a
Claudiu Belu <claudiu.belu@canonical.com>
2025-02-13 13:57:35 +0000

    Allow multiple local path snaps to be installed
    In airgapped environments, multiple snaps may be downloaded and then
    installed from local paths. The k8s snap depends on core20 snap.
    
    With this change, /capi/etc/snap-local-paths may contain multiple paths
    for local snaps to install, delimited by spaces.

--- pkg/cloudinit/scripts/install.sh
+++ pkg/cloudinit/scripts/install.sh
@@ -3,7 +3,8 @@
 ## Assumptions:
 ## - /capi/etc/snap-channel contains the snap channel to be installed that matches the desired Kubernetes version, e.g. "v1.30.1" -> "1.30-classic/stable"
 ## - /capi/etc/snap-revision contains the snap revision to be installed, e.g. 123
-## - /capi/etc/snap-local-path contains the path to the local snap file to be installed, e.g. /path/to/k8s.snap
+## - /capi/etc/snap-local-paths contains a space-separated list of local paths to snap files
+##   to be installed, e.g.: /path/to/core20.snap /path/to/k8s.snap
 
 if [ -f "/capi/etc/snap-channel" ]; then
   snap_channel="$(cat /capi/etc/snap-channel)"
@@ -11,9 +12,9 @@ if [ -f "/capi/etc/snap-channel" ]; then
 elif [ -f "/capi/etc/snap-revision" ]; then
   snap_revision="$(cat /capi/etc/snap-revision)"
   snap install k8s --classic --revision "${snap_revision}"
-elif [ -f "/capi/etc/snap-local-path" ]; then
-  snap_local_path="$(cat /capi/etc/snap-local-path)"
-  snap install --classic --dangerous "${snap_local_path}"
+elif [ -f "/capi/etc/snap-local-paths" ]; then
+  snap_local_paths="$(cat /capi/etc/snap-local-paths)"
+  snap install --classic --dangerous ${snap_local_paths}
 else
   echo "No snap installation option found"
   exit 1
