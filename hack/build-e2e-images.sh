db429c7d4084d5a9b9df049f6f0ac3f36f9c05ae
Claudiu Belu <claudiu.belu@canonical.com>
2025-05-06 17:45:23 +0000

    ci: Update the newer Kubernetes version to v1.33.0
    k8s-snap currently does not support patch-level upgrades (if a new node
    joins that has a different patch number, it will be rejected).
    
    Bump the newer Kubernetes version to v1.33.0 as a result. This should
    allow the integration test "Workload cluster upgrade" to pass.

--- hack/build-e2e-images.sh
+++ hack/build-e2e-images.sh
@@ -10,6 +10,6 @@ set -xe
 DIR="$(realpath "$(dirname "${0}")")"
 
 cd "${DIR}/../templates/docker"
-sudo docker build . -t k8s-snap:dev-old --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.32.1 --build-arg KUBERNETES_VERSION_UPGRADE_TO=v1.32.2
-sudo docker build . -t k8s-snap:dev-new --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.32.2
+sudo docker build . -t k8s-snap:dev-old --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.32.1 --build-arg KUBERNETES_VERSION_UPGRADE_TO=v1.33.0
+sudo docker build . -t k8s-snap:dev-new --build-arg BRANCH=main --build-arg KUBERNETES_VERSION=v1.33.0
 cd -
